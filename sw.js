5ed7757a62e15411559b587d56b2e10428ff1d83
shimi658 <sh8517843@gmail.com>
2026-07-17 13:53:44 +0300

    Refresh PWA cache for biometric sign-in

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = "contractor-expenses-v25";
+const CACHE_NAME = "contractor-expenses-v26";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./app-icon.svg", "./construction-background.svg"];
 
 self.addEventListener("install", (event) => {
