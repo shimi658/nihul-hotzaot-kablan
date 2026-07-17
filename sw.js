53c7c1f458616e0c19d22765542800b4d22f5d4d
shimi658 <sh8517843@gmail.com>
2026-07-17 18:07:00 +0300

    Refresh cache for new app icon

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
-const CACHE_NAME = "contractor-expenses-v37";
+const CACHE_NAME = "contractor-expenses-v38";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./styles-menu-v1.css", "./app-menu-v1.js", "./styles-menu-v2.css", "./app-menu-v2.js", "./styles-menu-v3.css", "./styles-menu-v4.css", "./styles-menu-v5.css", "./styles-menu-v6.css", "./styles-menu-v7.css", "./styles-menu-v8.css", "./app-menu-v7.js", "./app-menu-v6.js", "./app-menu-v3.js", "./app-menu-v4.js", "./app-menu-v5.js", "./manifest.webmanifest", "./app-icon.svg", "./construction-background.svg"];
 
 self.addEventListener("install", (event) => {
@@ -15,4 +14,3 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
   self.clients.claim();
 });
-
